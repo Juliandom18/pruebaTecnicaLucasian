24142515d4f0639c61f8ee2073bcfe1b39820315
= <=>
2024-09-06 09:10:54 -0500

    Conexión

---
+++ ejemplo.java
@@ -0,0 +1,11 @@
+public class ejemplo {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 0;
+        while (y < x) {
+            y += x;
+        }
+        System.out.println(y);
+    }
+    
+}
